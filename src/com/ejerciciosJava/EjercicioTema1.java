61dc72700a0258306475082ea0246f517d9b5066
Carla <barrientoscarla190@gmail.com>
2023-01-19 11:09:18 -0300

    Initial commit

---
+++ src/com/ejerciciosJava/EjercicioTema1.java
@@ -0,0 +1,17 @@
+package com.ejerciciosJava;
+
+public class EjercicioTema1 {
+    public static void main(String[] args) {
+        int edadPadre = 40;
+        long asistentes = 100;
+        double precioPantalon = 45.9d;
+        boolean colorFavoritoNegro = true;
+        String coloresBanderaDeJapon = "blanco y rojo";
+
+        System.out.println(edadPadre);
+        System.out.println(asistentes);
+        System.out.println(precioPantalon);
+        System.out.println(colorFavoritoNegro);
+        System.out.println(coloresBanderaDeJapon);
+    }
+}
