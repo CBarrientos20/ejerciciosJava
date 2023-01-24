17abaafe96e3d32a3e705acb4ecfeb8c630747c7
Carla <barrientoscarla190@gmail.com>
2023-01-24 09:50:22 -0300

    "Entregando ejercicio estructuras de control"

---
+++ src/com/ejerciciosJava/EjercicioTema3.java
@@ -0,0 +1,18 @@
+package com.ejerciciosJava;
+
+import com.sun.tools.javac.Main;
+
+public class EjercicioTema3 {
+    public static void main(String[] args) {
+
+        String[] productos = {"Coca-Cola", "Pepsi", "Fanta", "Paso de los Toros", "Sprite", "7-Up"};
+
+for(String nombre : productos){
+    System.out.print(nombre + "," + " ");
+}
+
+       // for (int i = 0; i < productos.length; i++){
+          //  System.out.print(productos[i] + " ");
+        //}
+    }
+}
